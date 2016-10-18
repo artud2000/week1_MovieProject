c8885faa2bdf07457208107d3745b313361cc012
Arturo Fernandez <aferathlon@gmail.com>
2016-10-17 20:03:39 -0700

    Initial Commit

---
+++ Week1_MovieProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Week1_MovieProject
+//
+//  Created by Arturo Fernandez on 10/17/16.
+//  Copyright © 2016 Arturo Fernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
